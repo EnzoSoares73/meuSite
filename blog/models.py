35fcfe71b10f7ebcbe9ace2b163e267da943ac05
EnzoSoares <enzosoutosoares@gmail.com>
2021-07-30 00:45:55 -0300

    Foram adicionados métodos novos ao modelo Post e ao arquivo admin para melhorar sua exibição na página admin.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
+
 
 class Post(models.Model):
     pub_date = models.DateTimeField("Data de publicação")
@@ -11,7 +12,41 @@ class Post(models.Model):
         return self.title
 
     def time_to_be_published(self):
-        if self.pub_date > datetime.now():
-            return self.pub_date - datetime.now()
+        if self.pub_date > datetime.now(timezone(timedelta(hours=-3))):
+            temp = self.pub_date - datetime.now(timezone(timedelta(hours=-3)))
+            str = self.format_timedelta(temp)
+            return f"Será publicado em {str}"
         else:
-            return f"Já publicado em {self.pub_date}"
+            return "Já publicado"
+
+    def format_timedelta(self, timedeltatobeformatted):
+        var = timedeltatobeformatted.total_seconds()
+
+        days = var // (24*60*60)
+        var -= days * (24*60*60)
+        hours = var // (60*60)
+        var -= hours * (60*60)
+        minutes = var // 60
+
+        str = f"{int(days)}"
+
+        if days == 1:
+            str = str + " dia "
+        else:
+            str = str + " dias, "
+
+        str = str+ f"{int(hours)}"
+
+        if hours == 1:
+            str = str + " hora "
+        else:
+            str = str + " horas e "
+
+        str = str + f"{int(minutes)}"
+
+        if minutes == 1:
+            str = str + " minuto "
+        else:
+            str = str + " minutos"
+
+        return str
