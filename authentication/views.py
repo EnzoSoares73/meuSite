6af3c64d3078b46a9a3f8812e8117746bb7c4a9d
EnzoSoares73 <enzosoutosoares@gmail.com>
2021-08-04 02:14:48 -0300

    Seção de blog da homepage adicionada

--- authentication/views.py
+++ authentication/views.py
@@ -5,12 +5,15 @@ from authentication.models import User
 
 
 def home(request):
-    num_blog_posts = 5
+    num_blog_posts = 2
     lista_blog_posts = Post.objects.order_by('-pub_date')[:num_blog_posts]
+    post1 = lista_blog_posts[0]
+    post2 = lista_blog_posts[1]
     user = User.objects.get(username="EnzoSoares")
     context = {
-        'lista_blog_posts': lista_blog_posts,
-        'user': user,
+        'post1': post1,
+        'post2': post2,
+        'user': user
     }
 
     return render(request, 'authentication/home.html', context)
