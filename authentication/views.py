e3a2506493e69410ae3c0da0c96fd0c9ddb6ab70
EnzoSoares73 <enzosoutosoares@gmail.com>
2021-08-06 00:47:44 -0300

    def truncate

--- authentication/views.py
+++ authentication/views.py
@@ -8,8 +8,11 @@ def home(request):
     num_blog_posts = 2
     lista_blog_posts = Post.return_published_posts(num=num_blog_posts)
     post1 = lista_blog_posts[0]
+    post1.text = truncate(post1.text)
     post2 = lista_blog_posts[1]
+    post2.text = truncate(post2.text)
     user = User.objects.get(username="EnzoSoares")
+    user.about = truncate(user.about)
     context = {
         'post1': post1,
         'post2': post2,
@@ -17,3 +20,16 @@ def home(request):
     }
 
     return render(request, 'authentication/home.html', context)
+
+def truncate(str, num_chars = 384):
+    special_chars = {'~', ':', "'", '+', '[', '\\', '@', '^', '{', '%', '(', '-', '"', '*', '|', ',', '&', '<', '`', '}', '.', '_', '=',
+     ']', '!', '>', ';', '?', '#', '$', ')', '/', ' '}
+
+    str = str[:num_chars]
+
+    while str[-1] in special_chars:
+        str = str[:-1]
+
+    str += '...'
+
+    return str
