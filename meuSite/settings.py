b64a8328035b929c5fe143a7fc16eec889147d05
EnzoSoares73 <enzosoutosoares@gmail.com>
2021-08-14 03:29:56 -0300

    Avançando no front de about.html

--- meuSite/settings.py
+++ meuSite/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mathfilters',
     'blog.apps.BlogConfig',
     'authentication.apps.AuthenticationConfig'
 ]
