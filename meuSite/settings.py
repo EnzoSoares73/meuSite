eb6f93dc9d9a4eecae36151de03f893d0494430c
EnzoSoares73 <enzosoutosoares@gmail.com>
2021-08-15 02:19:09 -0300

    .env criado

--- meuSite/settings.py
+++ meuSite/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-f = open('.env.dev', 'r')
+f = open('.env', 'r')
 newDict = {}
 for line in f:
     listedline = line.strip().split('=')
