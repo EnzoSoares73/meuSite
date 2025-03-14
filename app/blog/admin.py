135419b1cfee1de6df0c780e40bc43e41b4b5ea3
enzo.vieira <enzo.vieira@hep.solutions>
2025-03-13 22:09:39 -0300

    Subtítulo adicionado ao post

--- app/blog/admin.py
+++ app/blog/admin.py
@@ -11,7 +11,7 @@ class PostAdmin(admin.ModelAdmin):
             'fields': ('user', 'pub_date')
         }),
         ('Texto', {
-            'fields': ['title', 'text']
+            'fields': ['title', 'subtitle', 'text']
         }),
     )
 
